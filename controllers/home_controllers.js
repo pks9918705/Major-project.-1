8e94fd1a2f5d82bc3f01c08d66dc08088345cc01
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-02 12:36:31 +0530

    view engine is set up

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,6 +1,15 @@
 module.exports.home=function(req,res){
 
-    return res.end('<h1>Express is up for Codeial</h1>')
+    //? sending directly to browser 
+    // return res.end('<h1>Express is up for Codeial</h1>')
+
+    return res.render(
+        'home',
+        {
+            title: 'Home'
+        }   
+
+    )
 
 }
 
