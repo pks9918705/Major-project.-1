06df9bc832e836a9108d484f4a4cbd257e1195ac
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-17 08:34:02 +0530

    new- file uploading feature added

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -30,22 +30,57 @@ module.exports.profile = function (req, res) {
 
 //updating profile data
 
-module.exports.Update = function (req, res) {
+module.exports.Update =async function (req, res) {
     // we have to check that current_user.id should be same who editted the profile form otherwise anyone can put id and update someone's other profile 
 
     // if the id property of the user object in the request (req.user.id) is equal to the id parameter in the request URL (req.params.id). It is comparing whether the ID of the currently authenticated user (retrieved from req.user.id) matches the ID specified in the URL.
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id,req.body)
-        .then( (updatedUser) => {
-            console.log('Your profile has been updated',updatedUser);
+    //    let updatedUser=
+        // .then( (updatedUser) => {
+        //     console.log('Your profile has been updated',updatedUser);
+        //     return res.redirect('back')
+            
+        // })
+        try{
+
+            //finding the user
+            let user=await User.findByIdAndUpdate(req.params.id,req.body)
+            User.uploadAvatar(req,res,function(error){
+                if(error){
+                    console.log('******mutler error:',error)
+                }
+                // console.log(req.file);
+                user.name=req.body.name
+                user.email=req.body.email
+
+                //when user is uploaded a file
+                if(req.file)
+                {
+                    // this is saving the uploaded the file path in the avatar field of user 
+                    user.avatar=User.avatarPath + '/' + req.file.filename
+
+                }
+                user.save()
+                return res.redirect('back')
+                
+            })
+
+
+
+
+        }
+        catch(err){
+
+            req.flash("error",err)
             return res.redirect('back')
-            
-        })
-        .catch( (err) => {
-            console.log('Hato bhencho nhi hoga update ',err);
-           return res.status(401).send('Unauthorized')
-            
-        })
+
+        }
+        
+        
+    }
+    else{
+        console.log('Hato bhencho nhi hoga update ',err);
+        return res.status(401).send('Unauthorized')
     }
 }
 
--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 //creating scheme
 const mongoose = require('mongoose')
 
+//using multer
+const multer = require('multer')
+
+const path = require('path')
+const AVATAR_PATH=path.join('/uploads/users/avatars')
+
 const userSchema =new mongoose.Schema({
 
     // email and password are the main things for authentication
@@ -16,6 +22,9 @@ const userSchema =new mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    avatar:{
+        type:String
     }
 
 
@@ -24,6 +33,27 @@ const userSchema =new mongoose.Schema({
     timestamps:true
     // it has data of time when objects is created 
 });
+
+
+//code for multer storage
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH))
+      //  null and after that location where the file should be stored
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  //static 
+  userSchema.statics.uploadAvatar=multer({storage:storage}).single('avatar')
+  // here we are not going to send array of files and for one file single() is used
+
+  userSchema.statics.avatarPath=AVATAR_PATH
+
+
 
 
 const User= mongoose.model('User',userSchema);
